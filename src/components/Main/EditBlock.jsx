df9506785a588c07ead3b4091c073d21cc634329
bismarkboateng <bismarkb609@gmail.com>
2023-03-30 12:26:15 +0000

    nn

---
+++ src/components/Main/EditBlock.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import {MdOutlineKeyboardArrowDown, MdKeyboardArrowRight} from 'react-icons/md';
+import { RiDeleteBinLine, RiAddCircleLine } from 'react-icons/ri';
+
+
+const EditBlock = () => {
+  const [ toggleIcon, setToggleIcon ] = React.useState(false);
+
+  return (
+    <div className='acme__main'>
+      <div className='acme__main-nav'></div>
+
+        <div>
+          <h4 class="acme__main-title">Making the world a better place</h4>
+        </div>
+
+        <div className='acme__main-content'>
+          <article className='acme__main-content_ArticleA'>
+
+            <div className='acme__main-content_text'>
+              <p>
+                {toggleIcon
+                ? <MdOutlineKeyboardArrowDown  onClick={() => setToggleIcon(false)}/> 
+                : <MdKeyboardArrowRight  onClick={() => setToggleIcon(true)}/>
+                }
+                <span>Text</span>
+              </p>
+    
+              <RiDeleteBinLine className='del__icon'/>
+            </div>
+
+            <div className='acme__main-content__blogA'>
+                <h3>Making the world a better place</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Quasi accusamus quidem, nam aut omnis reprehenderit harum. Blanditiis 
+                accusantium dolorem exercitationem rem, harum saepe perspiciatis!</p>
+            </div>
+
+          </article>
+
+          <article className='acme__main-content_ArticleB'>
+            <div className='acme__main-content_image'>
+              <p>
+                {
+                toggleIcon 
+                ? <MdOutlineKeyboardArrowDown  onClick={() => setToggleIcon(false)}/> 
+                : <MdKeyboardArrowRight  onClick={() => setToggleIcon(true)}/>
+                }
+                <span>Image</span>
+              </p>
+              <RiDeleteBinLine className='del__icon'/>
+            </div>
+    
+            <div className='acme__main-content__blogB'>
+
+              <div className="acme__main-content_upload-image">
+                <p>Upload image</p>
+                <label htmlFor="file">
+                  <input type="file"  id="file"/>
+                  <span>Upload a file</span> or drag and drop
+                </label>
+              </div>
+
+              <div className="acme__main-content_upload-caption">
+                <p>Caption</p>
+                <input type="text" />  
+              </div>
+            </div>
+
+          </article>
+          <p className='acme__main-add_block' 
+                onClick={() => {}}>
+                <RiAddCircleLine />Add block
+          </p>
+      </div>
+
+    </div>
+  );
+
+}
+
+export default EditBlock
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,60 +1,14 @@
 import React, {useState} from 'react';
 import './main.css';
-import {MdOutlineKeyboardArrowDown, MdKeyboardArrowRight} from 'react-icons/md';
-import {RiAddCircleLine, RiDeleteBinLine} from 'react-icons/ri';
+import EditBlock from './EditBlock';
 
 
-const Main = () => {
-  const [toggleIcon, setToggleIcon] = useState(true);
-  const addBlock = ()=>{
-    alert("hey")
-  }
+const Main = () => (
+  <>
+    <EditBlock />
+  </>
+)
 
-  return (
-    <div className='acme__main'>
-       <div className='acme__main-nav'></div>
 
-       <div>
-         <h4 class="acme__main-title">Making the world a better place</h4>
-       </div>
 
-       <div className='acme__main-content'>
-       <article className='acme__main-content_ArticleA'>
-        <div className='acme__main-content_text'>
-        <p>{toggleIcon ? <MdOutlineKeyboardArrowDown  onClick={() => setToggleIcon(false)}/> 
-        : <MdKeyboardArrowRight  onClick={() => setToggleIcon(true)}/>} <span>Text</span></p>
-        
-        <RiDeleteBinLine className='del__icon'/>
-        </div>
-        <div className='acme__main-content__blogA'>
-            <h3>Making the world a better place</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi accusamus quidem, nam aut omnis reprehenderit harum. Blanditiis accusantium dolorem exercitationem rem, harum saepe perspiciatis!</p>
-          </div>
-        </article>
-
-          <article className='acme__main-content_ArticleB'>
-          <div className='acme__main-content_image'>
-          <p> {toggleIcon ? <MdOutlineKeyboardArrowDown  onClick={() => setToggleIcon(false)}/> : <MdKeyboardArrowRight  onClick={() => setToggleIcon(true)}/>}
-           <span>Image</span></p>
-           <RiDeleteBinLine className='del__icon'/>
-        </div>
-   
-             <div className='acme__main-content__blogB'>
-             <div className="acme__main-content_upload-image">
-               <p>Upload image</p>
-               <label htmlFor="file"><input type="file"  id="file"/> <span>Upload a file</span> or drag and drop</label>
-             </div>
-             <div className="acme__main-content_upload-caption">
-               <p>Caption</p>
-             <input type="text" />  
-             </div>
-         </div>
-          
-          </article>
-          <p className='acme__main-add_block' onClick={addBlock}><RiAddCircleLine />Add block</p>
-     </div>
-    </div>
-  )
-}
-
-export default Main
+export default Main;
