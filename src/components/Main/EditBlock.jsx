7ea30bb98bd0622d4598a8319819a7a54c7b5e0e
estherlardze <estherlardze8@gmail.com>
2023-03-30 23:15:50 +0000

    collapsible

--- src/components/Main/EditBlock.jsx
+++ src/components/Main/EditBlock.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {useCollapse} from 'react-collapsed';
 import {MdOutlineKeyboardArrowDown, MdKeyboardArrowRight} from 'react-icons/md';
 import { RiDeleteBinLine, RiAddCircleLine } from 'react-icons/ri';
 
 
 const EditBlock = () => {
-  const [ toggleIcon, setToggleIcon ] = React.useState(false);
+
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
 
   return (
     <div className='acme__main'>
@@ -17,49 +19,36 @@ const EditBlock = () => {
         <div className='acme__main-content'>
           <article className='acme__main-content_ArticleA'>
 
-            <div className='acme__main-content_text'>
+            <div className='acme__main-content_text'  {...getToggleProps()}>
               <p>
-                {toggleIcon
-                ? <MdOutlineKeyboardArrowDown  onClick={() => setToggleIcon(false)}/> 
-                : <MdKeyboardArrowRight  onClick={() => setToggleIcon(true)}/>
-                }
+              {isExpanded ? <MdOutlineKeyboardArrowDown /> : <MdKeyboardArrowRight />}
                 <span>Text</span>
               </p>
-    
               <RiDeleteBinLine className='del__icon'/>
             </div>
 
-              {toggleIcon && 
-               <div className='acme__main-content__blogA'>
+            <div className='acme__main-content__blogA' {...getCollapseProps()}>
                <h3>Making the world a better place</h3>
                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-               Quasi accusamus quidem, nam aut omnis reprehenderit harum. Blanditiis 
-               accusantium dolorem exercitationem rem, harum saepe perspiciatis!</p>
+                Quasi accusamus quidem, nam aut omnis reprehenderit harum. Blanditiis 
+                accusantium dolorem exercitationem rem, harum saepe perspiciatis!</p>
             </div>
-             }
-
-
+             
           </article>
 
           <article className='acme__main-content_ArticleB'>
-            <div className='acme__main-content_image'>
+            <div className='acme__main-content_image' {...getToggleProps()}>
               <p>
-                {
-                toggleIcon 
-                ? <MdOutlineKeyboardArrowDown  onClick={() => setToggleIcon(false)}/> 
-                : <MdKeyboardArrowRight  onClick={() => setToggleIcon(true)}/>
-                }
+              {isExpanded ? <MdOutlineKeyboardArrowDown /> : <MdKeyboardArrowRight />}
                 <span>Image</span>
               </p>
               <RiDeleteBinLine className='del__icon'/>
             </div>
 
-            {toggleIcon && 
-                <div className='acme__main-content__blogB'>
-
-                <div className="acme__main-content_upload-image">
-                  <p>Upload image</p>
-                  <label htmlFor="file">
+                <div className='acme__main-content__blogB' {...getCollapseProps()}>
+                    <div className="acme__main-content_upload-image">
+                   <p>Upload image</p>
+                   <label htmlFor="file">
                     <input type="file"  id="file"/>
                     <span>Upload a file</span> or drag and drop
                   </label>
@@ -70,7 +59,7 @@ const EditBlock = () => {
                   <input type="text" />  
                 </div>
               </div>
-             }
+             
 
           </article>
           <p className='acme__main-add_block' 
